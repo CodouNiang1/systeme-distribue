fc1297ac3ac384e7ed4836fdd90b34dd8fdbaab3
Codou Niang <niangcodou828@gmail.com>
2026-05-11 11:40:30 +0000

    CORBA embarqué dans Spring Boot pour Render

--- pdf-client-web/backend/src/main/java/api/CORBAService.java
+++ pdf-client-web/backend/src/main/java/api/CORBAService.java
@@ -3,22 +3,18 @@ package api;
 import org.omg.CORBA.ORB;
 import org.omg.CosNaming.NamingContextExt;
 import org.omg.CosNaming.NamingContextExtHelper;
-import org.springframework.beans.factory.annotation.Value;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAHelper;
 import org.springframework.stereotype.Service;
 import PDFService.IPDFService;
 import PDFService.IPDFServiceHelper;
+import servant.PDFServiceImpl;
 
 import javax.annotation.PostConstruct;
 import java.util.Properties;
 
 @Service
 public class CORBAService {
-
-    @Value("${corba.host:localhost}")
-    private String corbHost;
-
-    @Value("${corba.port:1050}")
-    private String corbaPort;
 
     private IPDFService service;
 
@@ -29,16 +25,36 @@ public class CORBAService {
             "com.sun.corba.ee.impl.orb.ORBImpl");
         props.setProperty("org.omg.CORBA.ORBSingletonClass",
             "com.sun.corba.ee.impl.orb.ORBSingleton");
-        props.setProperty("org.omg.CORBA.ORBInitialHost", corbHost);
-        props.setProperty("org.omg.CORBA.ORBInitialPort", corbaPort);
+        props.setProperty("org.omg.CORBA.ORBInitialHost", "localhost");
+        props.setProperty("org.omg.CORBA.ORBInitialPort", "1050");
 
         ORB orb = ORB.init(new String[]{}, props);
+
+        // Démarrer le NameService embarqué
+        com.sun.corba.ee.impl.naming.cosnaming.TransientNameService tns =
+            new com.sun.corba.ee.impl.naming.cosnaming.TransientNameService(
+                (com.sun.corba.ee.spi.orb.ORB) orb);
+
+        // Démarrer le POA
+        POA rootPOA = POAHelper.narrow(
+            orb.resolve_initial_references("RootPOA"));
+        rootPOA.the_POAManager().activate();
+
+        // Enregistrer le servant PDFService
+        PDFServiceImpl servant = new PDFServiceImpl();
+        org.omg.CORBA.Object ref = rootPOA.servant_to_reference(servant);
+        IPDFService pdfRef = IPDFServiceHelper.narrow(ref);
+
         NamingContextExt ns = NamingContextExtHelper.narrow(
-            orb.resolve_initial_references("NameService")
-        );
-        service = IPDFServiceHelper.narrow(ns.resolve_str("PDFService"));
-        System.out.println("[SPRING] Connecté au serveur CORBA "
-            + corbHost + ":" + corbaPort);
+            orb.resolve_initial_references("NameService"));
+        ns.rebind(ns.to_name("PDFService"), pdfRef);
+
+        service = pdfRef;
+
+        // Lancer l'ORB dans un thread séparé
+        new Thread(() -> orb.run()).start();
+
+        System.out.println("[SPRING] Serveur CORBA embarqué démarré !");
     }
 
     public IPDFService getService() {
